c7f48a4cdd07a57153b76528229ebaee58885185
milah <i1314900@continental.edu.pe>
2018-06-11 19:02:39 -0500

    Primer Commit

---
+++ App1/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnAuth;
+        EditText editTextUserName;
+        EditText editTextPassword;
+        EditText token;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btnAuth = FindViewById<Button>(Resource.Id.btnAuth);
+            editTextUserName = FindViewById<EditText>(Resource.Id.editTextUserName);
+            editTextPassword = FindViewById<EditText>(Resource.Id.editTextPassword);
+            editTextPassword = FindViewById<EditText>(Resource.Id.token);
+            btnAuth.Click += DoAuthentication;
+        }
+
+        private void DoAuthentication(object sender, EventArgs e)
+        {
+            if (editTextUserName.Text.Equals("bichito") && editTextUserName.Text.Equals("123456"))
+            {
+                Toast.MakeText(this, "Usuario Valido", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "Usuario Invalido, el teléfono se destruirá", ToastLength.Short);
+            }
+
+        }
+    }
+}
+
